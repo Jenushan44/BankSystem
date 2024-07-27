810261b53c7eb000a0528a5f1b343f258a6a584d
Jenushan Premkumar <jenushan447@gmail.com>
2024-07-27 15:02:39 -0400

    Added function to check balance

--- bankingManager.cpp
+++ bankingManager.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+void checkBalance(double balance);
+
 int main() {
+
+    int choice;
+    double balance = 0.0;
+
+    std::cout<<"Welcome to the Bank Manager\n";
+    std::cout<<"What would you like to do?\n";
+    std::cout<<"1. Check Balance";
+    std::cout<<"2. Make a Deposit";
+    std::cout<<"3. Make a Withdrawal";
+    std::cout<<"4. Exit";
+
+    std::cin>>choice;
+
+    switch(choice) {
+        case 1:
+            checkBalance(balance);
+    }
     
+
+
+
 }
+
+void checkBalance(double balance) {
+    std::cout<<"Your balance is " << balance;
+}
+
