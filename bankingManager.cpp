0dc0ab87b8ba84852795cd5eaec8bb99f123f225
Jenushan Premkumar <jenushan447@gmail.com>
2024-07-27 15:20:32 -0400

    makeDeposit function added

--- bankingManager.cpp
+++ bankingManager.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void checkBalance(double balance);
+double makeDeposit(double balance);
 
 int main() {
 
@@ -9,24 +10,34 @@ int main() {
 
     std::cout<<"Welcome to the Bank Manager\n";
     std::cout<<"What would you like to do?\n";
-    std::cout<<"1. Check Balance";
-    std::cout<<"2. Make a Deposit";
-    std::cout<<"3. Make a Withdrawal";
-    std::cout<<"4. Exit";
+    std::cout<<"1. Check Balance\n";
+    std::cout<<"2. Make a Deposit\n";
+    std::cout<<"3. Make a Withdrawal\n";
+    std::cout<<"4. Exit\n";
 
     std::cin>>choice;
 
     switch(choice) {
         case 1:
             checkBalance(balance);
-    }
-    
-
-
-
+            break;
+        case 2:
+            balance += makeDeposit(balance);
+            checkBalance(balance);
+            break;
+        
+        }
 }
 
 void checkBalance(double balance) {
     std::cout<<"Your balance is " << balance;
 }
 
+double makeDeposit(double balance) {
+    
+    double amount = 0.0;
+
+    std::cout<<"Please select the amount you would like to deposit: ";
+    std::cin>>amount;
+    return amount;
+}
