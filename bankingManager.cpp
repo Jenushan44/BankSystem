485756f6c59897726c10989aa5523c83ea1a9cb1
Jenushan Premkumar <jenushan447@gmail.com>
2024-07-27 15:27:52 -0400

    Added loop for using manager again

--- bankingManager.cpp
+++ bankingManager.cpp
@@ -10,6 +10,8 @@ int main() {
     double balance = 0.0;
 
     std::cout<<"Welcome to the Bank Manager\n";
+
+    do {
     std::cout<<"What would you like to do?\n";
     std::cout<<"1. Check Balance\n";
     std::cout<<"2. Make a Deposit\n";
@@ -31,10 +33,11 @@ int main() {
             checkBalance(balance);
             break;
         }
+    } while(choice != 4);
 }
 
 void checkBalance(double balance) {
-    std::cout<<"Your balance is " << balance;
+    std::cout<<"Your balance is " << balance << "\n";
 }
 
 double makeDeposit(double balance) {
